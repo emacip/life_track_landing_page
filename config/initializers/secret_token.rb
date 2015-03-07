7530f336dfe7fe24249a18f86a4135f92aa38392
emacip <emacip@eaglealpha.com>
2015-03-07 17:42:12 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeTrackLp::Application.config.secret_token = '661ac039ad55a8ec093e0b077ff0d3c452b78ff4c4cf150a9dda31cb9b0f80c9638fe4b5a91007cd1adfbcf11dce3e6989973067680c2061b811af53bc7d9b49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LifeTrackLp::Application.config.session_store :cookie_store, key: '_life_track_lp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LifeTrackLp::Application.config.session_store :active_record_store
